9dca53ce5a1ec27ea508986b74558e931ddb7696
mohsen90002 <mohsen90002@gmail.com>
2020-12-19 18:31:44 +0330

    firstFiles

---
+++ app/Controllers/Api.php
@@ -0,0 +1,369 @@
+<?php namespace App\Controllers;
+use CodeIgniter\HTTP\RequestInterface;
+
+class Api extends BaseController
+{
+    
+    private function showResult($code,$cont){
+        echo json_encode(array('code'=>$code,
+                               'content'=>$cont));
+        die();
+    }
+
+    public function getWelcomeScreen(){
+        $info = array(
+            'text' => "به فروشگاه محله خوش آمدید\nلطفا از منو  گزینه مناسب را انتخاب نمایید",
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        ); 
+        $this->showResult(200,$info) ;
+    }
+
+    public function getCategories(){
+        $info = array(
+            'text'=> 'تمامی دسته بندی هایی که در این بخش خواهید دید با محصولات دیده میشود',
+            'categories' => array(
+                array('title'=>'خرسی',
+                       'link' => base_url('Api/getCatProducts/1')),
+                array('title'=>'خرگوشی',
+                       'link' => base_url('Api/getCatProducts/2')),
+                array('title'=>'حوله ای',
+                       'link' => base_url('Api/getCatProducts/3')),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        $this->showResult(200,$info);
+    }
+
+    public function getCatProducts($cat=-1,$page=1){
+        $cat = intval($cat);
+        $page = intval($page);
+        if($cat <= 0 )
+            $this->showResult(404,'Invalid category id');
+        $page = $page <= 0 ? 1 : $page;
+
+        $info = array(
+            'text' => 'در این بخش تمامی کالاهای مربوط به این دسته بندی نمایش داده می شود',
+            'products' => array(
+                array('title'=> 'کیف خرسی شماره ۱', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => ('Api/getProductVarient/1') ),
+                array('title'=> 'کیف خرسی شماره ۲', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => ('Api/getProductVarient/2') ),
+                array('title'=> 'کیف خرسی شماره ۳', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => ('Api/getProductVarient/3') ),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>('getCategories')),
+                array('title'=>'تماس با ما', 'link'=>('contact')),
+                array('title'=>'جستجوی محصولات','link'=>('searchProducts')),
+                array('title'=>'سبد خرید','link'=>('basket')),
+                array('title'=>'پیگیری سفارش','link'=>('tracking')),
+            )
+        );
+        if($page > 2) $info['products'] = array();  
+        $this->showResult(200,$info);
+    }
+
+    public function topSellItems(){
+        $info = array(
+            'text' => 'در این بخش تمامی کالاهای مربوط به این دسته بندی نمایش داده می شود',
+            'products' => array(
+                array('title'=> 'کیف خرسی شماره ۱', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/getProductVarient/1') ),
+                array('title'=> 'کیف خرسی شماره ۲', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/getProductVarient/2') ),
+                array('title'=> 'کیف خرسی شماره ۳', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/getProductVarient/3') ),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+
+        $this->showResult(200,$info);
+    }
+
+    public function getspecificItems(){
+        $info = array(
+            'text' => 'در این بخش تمامی کالاهای مربوط به این دسته بندی نمایش داده می شود',
+            'products' => array(
+                array('title'=> 'کیف خرسی شماره ۱', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/getProductVarient/1') ),
+                array('title'=> 'کیف خرسی شماره ۲', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/getProductVarient/2') ),
+                array('title'=> 'کیف خرسی شماره ۳', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/getProductVarient/3') ),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        
+        $this->showResult(200,$info);
+    }
+
+    public function getProductVarient($pid=-1){
+        $pid = intval($pid);
+        if($pid <= 0 )
+            $this->showResult(404,'Invalid Product Id');
+
+        $info = array(
+            'text' => 'لطفا تنوع کالا را انتخاب کنید',
+            'varients' => array(
+                array('title'=> 'رنگ قرمز', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/addBasket/1/1') ),
+                array('title'=> 'رنگ آبی', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/addBasket/2/1') ),
+                array('title'=> 'رنگ سبز', 'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'link' => base_url('Api/addBasket/3/1') ),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        
+        $this->showResult(200,$info);
+    }
+
+    public function addBasket($pid=-1,$pvar=-1){
+        $pid = intval($pid);
+        if($pid <= 0 )
+            $this->showResult(404,'Invalid Product Id');
+        $pvar = intval($pid);
+        if($pvar <= 0 )
+            $this->showResult(404,'Invalid Product Varient');
+
+        $this->showResult(200,'Added Successfully');
+
+    }
+
+    public function showBasket(){
+
+        $info = array(
+            'text' => 'لیست خرید شما به شکل زیر می باشد',
+            'products' => array(
+                array('id'=>1, 
+                      'title'=> 'کیف خرسی ۱', 
+                      'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'varient' => 'رنگ قرمز ' , 
+                      'qnt' => 2, 
+                      'price' => 240000),
+                array('id'=>2, 
+                      'title'=> 'کیف خرسی ۲', 
+                      'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'varient' => 'رنگ آبی ' , 
+                      'qnt' => 1, 
+                      'price' => 44000),
+            ),
+            'totalqnt' => 3,
+            'totalprice' => 920000,
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        
+        $this->showResult(200,$info);
+
+    }
+
+    public function removeItem($bid=-1){
+        $bid = intval($bid);
+        if($bid <= 0)
+            $this->showResult(404,'Invalid product id in basket');
+        $this->showResult(200,'Item Removed Successfully');
+    }
+
+    public function getAddresses(){
+        $info = array(
+            'text' => 'لطفا آدرس خود را انتخاب کنید',
+            'addresses' => array(
+                array('title'=> 'خیابان پاسداران ، نبش  بلوار دوم ، ساختمان اداری ستاوه طبقه ۱۲ واحد ۳ - علی حمیدی', 
+                      'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      ),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        $this->showResult(200,$info);
+    }
+
+    public function addNewAddress(){
+        $lat  = $this->request->getPost('lat');
+        $long = $this->request->getPost('long');
+        $address = $this->request->getPost('address');
+        $recipient = $this->request->getPost('recipient');
+        if($lat == '' || $long == '' || $address == '' || $recipient == '')
+            $this->showResult(412,'Invalid enteries');
+        else
+            $this->showResult(200,'Adress added successfully');
+
+    }
+
+    public function getsendTypes(){
+        $info = array(
+            'text' => 'لطفا شیوه ارسال سفارش را انتخاب نمایید',
+            'sendtypes' => array(
+                array('title'=> 'ارسال پستی\n هزینه :‌ ۲۰ هزار تومان \n سه روزه' ,'link' => base_url('Api/selectSendType/1')),
+                array('title'=> 'ارسال تیپاکس\n هزینه :‌ ۳۰ هزار تومان \n ۱ روزه' ,'link' => base_url('Api/selectSendType/2'))
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        $this->showResult(200,$info);
+    }
+
+    public function selectSendType($stype=-1){
+        $stype = intval($stype);
+        if($stype <= 0 )
+            $this->showResult(404,'Invalid shipping type');
+
+        $this->showResult(412,'Send Type added successfully !');
+    }
+
+    public function showPayLink($ordId=-1){
+        $ordId = intval($ordId);
+        if($ordId <= 0)
+            $this->showResult(404,'Invalid Order Id');
+        $info = array(
+            'link' => 'http://shaparak.com/us/sdfsdf',
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+    }
+
+    public function contact(){
+        $info = array(
+            "text"=> "برای تماس با ما از راه های زیر میتوانید اقدام کنید \n info@gmail.com",
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        $this->showResult(200,$info);
+    }
+
+    public function searchProducts(){
+        $term = $this->request->getGet('q');
+        if($term == '')
+            $this->showResult(412,'Empty search term');
+        $info = array(
+            'text' => 'لیست جستجو شما به شکل زیر می باشد',
+            'products' => array(
+                array('id'=>1, 
+                      'title'=> 'کیف خرسی ۱', 
+                      'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'varient' => 'رنگ قرمز ' , 
+                      'price' => 240000,
+                      'link'  => base_url('Api/getProductVarient/1')
+                    ),
+                array('id'=>2, 
+                      'title'=> 'کیف خرسی ۲', 
+                      'picture'=> 'https://www.ninibazar.com/wp-content/uploads/2020/05/baby-bag-bear-1.jpg',
+                      'varient' => 'رنگ آبی ' , 
+                      'price' => 44000,
+                      'link'  => base_url('Api/getProductVarient/2')
+                    ),
+            ),
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        $this->showResult(200,$info);
+    }
+
+    public function tracking($trackno=-1){
+        $trackno = intval($trackno);
+        if($trackno <= 0 )
+            $this->showResult(404,'Invalid Tracking Number');
+
+        $info = array(
+            'text'=> 'کالای شما به پست فرستاده شده است و تا ۲۴ ساعت آینده به دست شما خواهد رسید',
+            'keyboard' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        );
+        $this->showResult(200,$info);
+    }
+
+    public function defaultMessage(){
+        $info = array(
+            'text' => "پیام شما مفهوم نیست لطفا از لیست زیر کمک بگیرید",
+            'default' => array(
+                array('title'=>'دسته محصولات','link'=>base_url('getCategories')),
+                array('title'=>'تماس با ما',
+                      'link'=>base_url('contact')),
+                array('title'=>'جستجوی محصولات','link'=>base_url('searchProducts')),
+                array('title'=>'سبد خرید','link'=>base_url('basket')),
+                array('title'=>'پیگیری سفارش','link'=>base_url('tracking')),
+            )
+        ); 
+        $this->showResult(200,$info) ;
+    }
+
+	public function index()
+	{
+		echo 'API Ver 1.0.';
+	}
+
+}
---
+++ app/Controllers/Telegram_controller.php
@@ -0,0 +1,202 @@
+<?php namespace App\Controllers;
+// use CodeIgniter\Controller;
+// use App\Libraries\Telegram;
+
+class Telegram_controller extends BaseController{
+
+	private $token = "1456393662:AAE6ckb3yoCRRoR9EOi-aB3GvcsRDhvTUJQ",
+			$offset_file = "offset.txt",
+			$url="https://api.telegram.org/bot1456393662:AAE6ckb3yoCRRoR9EOi-aB3GvcsRDhvTUJQ/";
+
+	public function index(){
+		echo 'hi';
+	}
+
+	private function loadUrl($url = "https://tg.kia24.com/publicn", $params=array()){
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		if(!empty($params)){
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		}
+		if (curl_errno($ch)) { 
+			print curl_error($ch); 
+		}
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+		curl_close($ch);
+		return $result; 
+	}
+
+	private function renderForTelegram($content, $data){
+		$result = array();
+		$result['data'] = $data;
+		if(isset($content->text)){
+			$text_reply = $content->text;
+			$result["text"] = $text_reply;
+		}
+		if(isset($content->keyboard)){
+			$keyboard = $this->renderKeyboard($content->keyboard);
+			$result["keyboard"] = $keyboard;
+		}
+		if(isset($content->default)){
+			$inlineKeyboard = $this->renderInlineButton($content->default);
+			$result["inlineKeyboard"] = $inlineKeyboard;
+		}
+		if(isset($content->products)){
+			$media = $this->renderMedia($content->products);
+			$result["media"] = json_encode($media);
+		}
+		return $result;
+	}
+	
+	private function renderKeyboard($content){
+		$final = array();
+		for($i = 0 ; $i < count($content); $i+=2){
+			$temp=array();
+			if(isset($content[$i])){
+				$temp[] = array($content[$i]->title);
+			};
+			if(isset($content[$i+1])){
+				$temp[] = array($content[$i+1]->title);
+			};
+			$final['keyboard'][] = $temp;
+		}
+		$final['keyboard']['one_time_keyboard'] = true;
+		$final['keyboard']['resize_keyboard'] = true;
+		return json_encode($final);;
+	}
+
+	public function renderInlineButton($content=''){
+		$keyboardArray = array();
+		for($i = 0 ; $i < count($content); $i+=2){
+			$temp=array();
+			if(isset($content[$i])){
+				$temp[] = array("text" => $content[$i]->title, "callback_data" => $content[$i]->link);
+			};
+			if(isset($content[$i+2])){
+				$temp[] = array("text" => $content[$i+1]->title, "callback_data" => $content[$i+1]->title);
+			};
+			$keyboardArray[] = $temp;
+		}
+		$inlineKeyboard = array(
+			"inline_keyboard" => $keyboardArray
+		);
+		$inlineKeyboard = json_encode($inlineKeyboard);
+		return $inlineKeyboard;
+	}
+
+	public function renderMedia($content=''){
+		$media = array();
+		$i = 0;
+		if(isset($content[$i])){
+			for($i = 0; $i < count($content); $i++){
+				$media[] = array('type' => 'photo', 'media' => $content[$i]->picture, 'caption' => $content[$i]->title, 'link' => $content[$i]->link);
+			}
+			// $media['reply_to_message_id'] = 
+		}
+		return $media;
+	}
+
+	public function getUpdates(){
+		echo(FCPATH.$this->offset_file);
+		$offset = file_exists(FCPATH.$this->offset_file) ? file_get_contents($this->offset_file) : 0;	
+		// var_dump($offset); die;
+		$result = $this->loadUrl($this->url."getUpdates?offset=".$offset);
+
+		// $data = $this->responseToMessage($result);
+
+			$this->responseToMessage($result);
+
+
+	}
+
+
+	public function replyToTelegram($data){
+		$chat_id = isset($data['data']['chat_id']) ? $data['data']['chat_id'] : '';
+		$text = isset($data['text']) ? str_replace(PHP_EOL, '', $data['text']) : '';
+		$keyboard = isset($data['keyboard']) ? str_replace(PHP_EOL, '', $data['keyboard']) : '';
+		$inlineKeyboard = isset($data['inlineKeyboard']) ? str_replace(PHP_EOL, '', $data['inlineKeyboard']) : '';
+		$media = isset($data['media']) ? $data['media'] : '';
+
+		
+		if(isset($Keyboard)){
+			$this->loadUrl("https://api.telegram.org/bot".$this->token."/sendMessage?chat_id=".$chat_id."&text=".$text."&ReplyKeyboardMarkup=".$keyboard);
+			exit();
+		}
+		if(isset($media) && !isset($keyboard)){
+			$this->loadUrl("https://api.telegram.org/bot".$this->token."/sendMediaGroup?chat_id=".$chat_id."&media=".$media);
+			exit();
+		}
+		if(isset($photo)){
+			$this->loadUrl("https://api.telegram.org/bot".$this->token."/sendMediaGroup?chat_id=".$chat_id."&photo=".$photo);
+			exit();
+		}			
+		if(isset($inlineKeyboard)){
+			$this->loadUrl("https://api.telegram.org/bot".$this->token."/sendMessage?chat_id=".$chat_id."&text=".$text."&reply_markup=".$inlineKeyboard);
+			exit();
+		}
+	}
+
+	public function responseToMessage($sendMessage){
+		$data = array();
+		$last_update_id = 0;
+		
+		foreach($sendMessage->result as $items){
+			var_dump($items);
+			if(isset($items->callback_query)){
+				$data['callback_query_id'] = $items->callback_query->id;
+				$data['text'] = $items->data;
+				$this->loadUrl($this->url."/answerCallbackQuery?callback_query_id=".$items->callback_query->id);
+				echo ('call back answered:'.$items->callback_query->id);
+			}
+			else{
+				// $data['text'] = isset($items->message->text) ? $items->message->text: '';
+				$data['text'] = isset($items->message->from->text) ? $items->message->from->text: '';
+			}
+
+			$data['update_id'] = isset($items->update_id) ? $items->update_id : '';
+			$data['message_id'] = isset($items->message->message_id) ? $items->message->message_id: '';
+			$data['from_id'] = isset($items->message->from->id) ? $items->message->from->id: '';
+			$data['first_name'] = isset($items->message->from->first_name) ?$items->message->from->first_name: '';
+			$data['username'] = isset($items->message->from->username) ? $items->message->from->username: '';
+			$data['chat_id'] =isset($items->message->chat->id) ? $items->message->chat->id: '';
+			$data['chat_type'] = isset($items->message->chat->type) ? $items->message->chat->type: '';
+			$data['date'] = isset($items->message->date) ? $items->message->date: '';
+			
+			$data['offset'] = isset($items->message->entities['0']->offset) ? $items->message->entities['0']->offset  : '';
+			$data['length'] = isset($items->message->entities['0']->length) ? $items->message->entities['0']->length : '';
+			$data['type'] = isset($items->message->entities['0']->type) ? $items->message->entities['0']->type : '';
+
+			// foreach($items->message->entities as $item){
+			// 	$data['entities']['offset'] = $item->offset;
+			// 	$data['entities']['length'] = $item->length;
+			// 	$data['entities']['type'] = $item->type;
+			// }
+
+			$apiUrl  = array(
+				'/start' =>  "https://tg.kia24.com/public/api/getWelcomeScreen",
+				'getCategories'=> "https://tg.kia24.com/public/api/getCategories" ,
+				'دسته محصولات'=> "https://tg.kia24.com/public/api/getCategories" ,
+				'getCatProducts'=> "https://tg.kia24.com/public/api/getCatProducts/1" ,
+				'topSellItems'=> "https://tg.kia24.com/public/api/topSellItems" ,
+				'topSellItems'=> "https://tg.kia24.com/public/api/topSellItems" ,
+			);
+
+			$renderedContent = $url = '';
+			if(isset($apiUrl[$data["text"]]))
+				$url = $apiUrl[$data["text"]];
+			else 
+				$url = "https://tg.kia24.com/public/api/defaultMessage";
+			
+			$content = $this->loadUrl($url);
+			$renderedContent = $this->renderForTelegram($content->content, $data);
+			$this->replyToTelegram($renderedContent);
+			$last_update_id = $items->update_id;
+			
+		}
+		echo ('updated');
+		file_put_contents(FCPATH.$this->offset_file, $last_update_id);
+		
+	}
+
+}
